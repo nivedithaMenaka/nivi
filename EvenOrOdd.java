5418f8c5380df47faed483657b14267348ab5804
nivedithaMenaka <nivibe96@gmail.com>
2017-07-06 12:07:48 +0530

    Create EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class EvenOrOdd
+{
+  
+  public static void main(String args[])
+  {
+    
+    int a;
+    Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+    if(a%2==0)
+    System.out.println(a+ " is a even number");
+else 
+System.out.println(a+" is a odd number");
+ 
+   }
+   }
